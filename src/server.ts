6c963304496a50ed2f9cf926a8ffcf4bd3893709
Morgana <morganabagatini@gmail.com>
2022-05-04 22:06:01 -0300

    get and post endpoind

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,28 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/feedbacks", async (req, res) => {
+  const data = await prisma.feedback.findMany();
+  return res.json({ data: data });
+});
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log("---> Server started on port 3333");

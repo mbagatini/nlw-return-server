a821cd134bef3c1b5fda1270a05f23119ecceb88
Morgana <morganabagatini@gmail.com>
2022-05-09 14:42:10 -0300

    migration

--- prisma/migrations/20220509162214_create_feedback/migration.sql
+++ prisma/migrations/20220509162214_create_feedback/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE `feedback` (
     `id` VARCHAR(191) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
     `comment` VARCHAR(191) NOT NULL,
-    `screenshot` VARCHAR(191) NULL,
+    `screenshot` TEXT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

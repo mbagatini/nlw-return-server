3b03df52e6a5d3b16bdceb95e43c68997ad4704b
Morgana <morganabagatini@gmail.com>
2022-05-09 12:14:48 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -14,5 +14,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("---> Server started on port 3333");
+  console.log("----> Server started on port 3333");
 });
